ec322117d15ae269fd12d69d22c1db1ac731ecc7
Rafael Alexandre Pardini Guedes <rafael.pardini@yahoo.com.br>
2020-09-13 13:37:24 -0300

    Implementando o Novo Projeto (Listas e Items) - PARTE 4

--- lists/tests.py
+++ lists/tests.py
@@ -5,24 +5,24 @@ from django.http import HttpRequest
 from lists.views import home_page
 from lists.models import Item
 
-class HomePageTest(TestCase):
-
-    def test_home_page_returns_correct_html(self):
-        response = self.client.get('/')
-        self.assertTemplateUsed(response, 'home.html')
+class NewListTest(TestCase):
 
     def test_can_save_a_POST_request(self):
-        response = self.client.post('/', data={'item_text': 'A new list item'})
-
+        self.client.post('/lists/new', data={'item_text': 'A new list item'})
         self.assertEqual(Item.objects.count(), 1)
         new_item = Item.objects.first()
         self.assertEqual(new_item.text, 'A new list item')
 
     def test_redirects_after_POST(self):
-        response = self.client.post('/', data={'item_text': 'A new list item'})
+        response = self.client.post('/lists/new', data={'item_text': 'A new list item'})
+        self.assertRedirects(response, '/lists/the-only-list-in-the-world/')
 
-        self.assertEqual(response.status_code, 302)
-        self.assertEqual(response['location'], '/lists/the-only-list-in-the-world/')
+
+class HomePageTest(TestCase):
+
+    def test_home_page_returns_correct_html(self):
+        response = self.client.get('/')
+        self.assertTemplateUsed(response, 'home.html')
 
     def test_only_saves_items_when_necessary(self):
         self.client.get('/')
